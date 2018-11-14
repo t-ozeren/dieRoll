449dd86794cd24b2f3fb1b4cee34cbf2d0e12b29
t-ozeren <ozerentolga@gmail.com>
2018-11-15 01:35:28 +0300

    Controll added

--- dice.py
+++ dice.py
@@ -7,22 +7,28 @@ def clrscreen():
 
 def rndm(num):
     rndm_num=random.randint(1,num)
-
     print(rndm_num)
+
+def control(key_input):
+    if key_input.isnumeric() == True:
+        return int(key_input)
+    else:
+        control(input("pls use only digits:"))
+
 def menu():
     while True:
         clrscreen()
-        key=input("""
+        key=control(input("""
     1-Roll a dice
     0-Quit
-    Choice: """)
-        if key is '1':
+    Choice: """))
+        if key is 1:
             clrscreen()
-            key_num=input("\nHow many side you want ? ")
+            key_num=control(input("\nHow many side you want ? "))
             print("Your result:",end=' ')
             rndm(int(key_num))
             input()
-        elif key is '0':
+        elif key is 0:
             clrscreen()
             break
         else:
